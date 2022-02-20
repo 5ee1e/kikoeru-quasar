68d7bdbce201883fe9125c68027139c4c3cfce85
asmr-one <88574818+asmr-one@users.noreply.github.com>
2022-02-20 12:40:31 +0000

    Update dist to output generated at 97c1e7c

---
+++ functions/work/[workID].js
@@ -0,0 +1,38 @@
+// example of workInfo
+// {"id":373001,"title":"【クーポンつき】彼女のおうちにお泊まり添い寝。【眠れる耳かき、ささやき、寝息といちゃあまマッサージ】","circle_id":46007,"name":"いちのや","nsfw":false,"release":"2022-02-16","dl_count":954,"price":1056,"review_count":1,"rate_count":237,"rate_average_2dp":4.91,"rate_count_detail":[{"review_point":1,"count":1,"ratio":0},{"review_point":2,"count":0,"ratio":0},{"review_point":3,"count":2,"ratio":0},{"review_point":4,"count":13,"ratio":5},{"review_point":5,"count":221,"ratio":93}],"rank":[{"term":"day","category":"all","rank":1,"rank_date":"2022-02-16"},{"term":"week","category":"all","rank":18,"rank_date":"2022-02-18"},{"term":"day","category":"voice","rank":1,"rank_date":"2022-02-16"},{"term":"week","category":"voice","rank":12,"rank_date":"2022-02-18"},{"term":"month","category":"voice","rank":79,"rank_date":"2022-02-17"}],"has_subtitle":false,"create_date":"2022-02-17","circle":{"id":46007,"name":"いちのや"},"vas":[{"id":"2b5e7ab5-d994-5491-a53c-f1b6ae562d0e","name":"一之瀬りと"}],"tags":[{"id":4,"name":"纯爱/甜蜜"},{"id":8,"name":"日常/生活"},{"id":13,"name":"温馨"},{"id":14,"name":"情侣"},{"id":56,"name":"治愈"},{"id":442,"name":"掏耳"},{"id":497,"name":"ASMR"},{"id":503,"name":"低语"}],"samCoverUrl":"https://api.asmr.one/api/cover/373001.jpg?type=sam","thumbnailCoverUrl":"https://api.asmr.one/api/cover/373001.jpg?type=240x240","mainCoverUrl":"https://api.asmr.one/api/cover/373001.jpg?type=main"}
+
+export async function onRequestGet(context) {
+  // Contents of context object
+  const {
+    params, // if filename includes [id] or [[path]]
+    next, // used for middleware or to fetch assets
+  } = context;
+
+  // Get the workID from the URL
+  const workID = params.id;
+
+  // request work info from api
+  const res = await fetch(`https://api.asmr.one/api/workInfo/${workID}`);
+  const workInfo = await res.json();
+
+  // build meta
+  const meta = `
+        <meta property="og:site_name" content="ASMR Online">
+        <meta property="og:url" content="https://www.asmr.one/work/${workID}">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="${workInfo.title}">
+        <meta property="og:description" content="
+        Price: ${workInfo.price} \n
+        Sales: ${workInfo.dl_count} \n\n
+
+        Circle: ${workInfo.circle.name} \n
+        Actors: ${workInfo.vas.map(v => v.name).join(', ')} \n
+        Release: ${workInfo.release} \n
+        ">
+        <meta property="og:image" content="${workInfo.mainCoverUrl}">
+  `
+
+  // render metadata to html
+  const html = await next();
+  return new Response(html.replace('<head>', `<head>${meta}`), res);
+}
